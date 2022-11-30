09005adb761f3fff4faec8c32b170d34450d99d1
Bonnet Ronan <rbonnet@insa-20705>
2022-11-30 16:22:37 +0100

    Create Organise Table

--- personnel.sql
+++ personnel.sql
@@ -1,2 +1,11 @@
 CREATE TABLE Personnel
 ();
+
+
+CREATE TABLE Organise (
+    idPortesOuverte INT NOT NULL,
+    idPersonnel INT NOT NULL,
+    FOREIGN KEY (idPortesOuverte) REFERENCES Journee_Portes_Ouvertes(idPorte_Ouverte),
+    FOREIGN KEY (idPersonnel) REFERENCES Personnel(idPersonnel),
+    PRIMARY KEY(idPortesOuverte, idPersonnel)
+);
