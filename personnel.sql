7878f7d25bedf049d2ef3777d0f06e0ddcec6dcf
Bonnet Ronan <rbonnet@insa-20705>
2022-11-30 16:48:39 +0100

    Fix oublies

--- personnel.sql
+++ personnel.sql
@@ -8,39 +8,50 @@ CREATE TABLE Personnel
     date_de_recrutement date,
 );
 
+CREATE Publication (
+    idPublication SERIAL NOT NULL,
+    titre TEXT,
+
+);
+
 CREATE Participe(
     idProjet INT NOT NULL,
     idScientifique INT NOT NULL,
     FOREIGN KEY (idProjet) REFERENCES Projet(idProjet),
-    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique)
+    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique),
+    PRIMARY KEY (idProjet, idScientifique)
 );
 
 CREATE Preside(
     idCongres INT NOT NULL,
     idScientifique INT NOT NULL,
     FOREIGN KEY (idCongres) REFERENCES Congres(idCongres),
-    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique)
+    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique),
+    PRIMARY KEY (idCongres, idScientifique)
 );
 
 CREATE Publie_Scientifique(
     idPublication INT NOT NULL,
     idScientifique INT NOT NULL,
     FOREIGN KEY (idPublication) REFERENCES Publication(idPublication),
-    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique)
+    FOREIGN KEY (idScientifique) REFERENCES Scientifique(idScientifique),
+    PRIMARY KEY (idPublication, idScientifique)
 );
 
 CREATE Publie_Doctorant(
     idPublication INT NOT NULL,
     idDoctorant INT NOT NULL,
     FOREIGN KEY (idPublication) REFERENCES Publication(idPublication),
-    FOREIGN KEY (idDoctorant) REFERENCES Doctorant(idDoctorant)
+    FOREIGN KEY (idDoctorant) REFERENCES Doctorant(idDoctorant),
+    PRIMARY KEY (idPublication, idDoctorant)
 );
 
 CREATE TABLE Publie_Externe(
     idPublication INT NOT NULL,
     idAuteurExterne INT NOT NULL,
     FOREIGN KEY (idPublication) REFERENCES Publication(idPublication),
-    FOREIGN KEY (idAuteurExterne) REFERENCES Auteur_Externe(idAuteur)
+    FOREIGN KEY (idAuteurExterne) REFERENCES Auteur_Externe(idAuteur),
+    PRIMARY KEY (idPublication, idAuteurExterne)
 
 );
 
@@ -48,7 +59,8 @@ CREATE TABLE Participe_Externe(
     idProjet INT NOT NULL,
     idPartenaire INT NOT NULL,
     FOREIGN KEY (idProjet) REFERENCES Projet(idProjet),
-    FOREIGN KEY (idPartenaire) REFERENCES Partenaire(idPartenaire)
+    FOREIGN KEY (idPartenaire) REFERENCES Partenaire(idPartenaire),
+    PRIMARY KEY (idProjet, idPartenaire)
 );
 
 CREATE TABLE Organise (
