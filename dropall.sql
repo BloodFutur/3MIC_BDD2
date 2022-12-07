aeaaea17315b7c44d8325d911f92920cf41a49b3
nbillard <nathanbillard12@gmail.com>
2022-12-07 09:01:04 +0100

    file to drop all tables

---
+++ dropall.sql
@@ -0,0 +1,17 @@
+DROP TABLE Personnel;
+DROP TABLE Doctorant;
+DROP TABLE Scientifique;
+DROP TABLE Enseignant_Chercheur;
+DROP TABLE Evenement;
+DROP TABLE Congres;
+DROP TABLE Labo_externe;
+DROP TABLE Journee_Porte_Ouvertes;
+DROP TABLE Auteur_Externe ;
+DROP TABLE Publication ;
+DROP TABLE Participe;
+DROP TABLE Preside;
+DROP TABLE Publie_Scientifique;
+DROP TABLE Publie_Doctorant;
+DROP TABLE Publie_Externe;
+DROP TABLE Participe_Externe;
+DROP TABLE Organise ;
