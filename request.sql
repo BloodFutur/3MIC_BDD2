55d3ca33ca4b6442c1d7fb988a77d86fec19e042
Olougouna Axel <olougoun@insa-20230>
2022-12-07 10:49:10 +0100

    question 4

--- request.sql
+++ request.sql
@@ -1,1 +1,10 @@
 -- Nathan
+
+
+--Axel, question 3 et 4
+--SELECT COUNT(id) 
+--FROM SELECT idScientifique FROM Enseignant_Chercheur
+--    WHERE idScientifique = 01;
+
+SELECT COUNT(idPublication) as nbCollab FROM Publication
+WHERE classeConf = 'A';
