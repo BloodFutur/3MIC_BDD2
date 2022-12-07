8d85315e8fd6240dd89fabb5417e2930249a9b64
Olougouna Axel <olougoun@insa-20230>
2022-12-07 10:27:04 +0100

    ajout d'etablissement

--- insert.sql
+++ insert.sql
@@ -6,3 +6,26 @@ INSERT INTO Personnel (
     'chez Nathan',
     '2020-9-15'
 );
+
+
+--Axel
+INSERT INTO Etablissement(
+    01,
+    'Institut National des Sciences Appliquées',
+    'INSA',
+    'Toulouse'
+);
+
+INSERT INTO Etablissement(
+    02,
+    "Ecole Nationale de l'Aviation Civile",
+    'ENAC',
+    'Toulouse'
+);
+
+INSERT INTO Etablissement(
+    03,
+    "Ecole Polytechnique",
+    "X",
+    'Paris'    
+);
