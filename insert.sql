da636061cee086df3efcdf658556e0efe136fe2b
nbillard <nathanbillard12@gmail.com>
2022-12-07 10:18:19 +0100

    plus de personnel

--- insert.sql
+++ insert.sql
@@ -6,3 +6,48 @@ INSERT INTO Personnel (
     'chez Nathan',
     '2020-9-15'
 );
+
+INSERT INTO Personnel (
+    1,
+    'Bonnet',
+    'Ronan',
+    DATE '2002-12-25',
+    'chez Ronan',
+    '2020-9-15'
+);
+
+INSERT INTO Personnel (
+    2,
+    'Olougouna',
+    'Axel',
+    DATE '2001-12-25',
+    'chez Axel',
+    '2020-9-15'
+);
+
+INSERT INTO Personnel (
+    3,
+    'Grane',
+    'Yasmine',
+    DATE '2001-12-25',
+    'chez Yasmine',
+    '2020-9-15'
+);
+
+INSERT INTO Personnel (
+    4,
+    'Wonderland',
+    'Alice',
+    DATE '1978-12-25',
+    'chez les lapins',
+    '2020-9-15'
+);
+
+INSERT INTO Personnel (
+    4,
+    'Le Bricoleur',
+    'Bob',
+    DATE '1878-12-25',
+    'au chantier',
+    '2020-9-15'
+);
